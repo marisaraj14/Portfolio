2f36370593d9ec7ffddeab4b83fec895bf7ecdeb
Marisa <81415318+marisaraj14@users.noreply.github.com>
2022-01-02 16:21:54 +0530

    Website usable

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,20 +1,13 @@
 export default function Contact() {
     return (
-        <section className="mt-20 w-screen ml-2 p-4 h-20" id="contact">
+        <section className="mt-20 w-screen p-4 h-26 bg-gray-200" id="contact">
             <header className="flex">
-                <p className="font-source-code font-bold text-4xl">contact me. &#60;</p>
+                <p className="font-source-code font-bold text-4xl">contact me.</p>
                 <a href="mailto:marisaraj14@gmail.com"><i className="fa fa-at text-3xl p-2 font-bold" title="Email me"></i></a>
                 <a href="https://github.com/marisaraj14" target="_blank"><i className="fa fa-github text-3xl p-2 font-bold" title="My Github repositiories"></i></a>
                 <a href="https://www.linkedin.com/in/marisaraj/" target="_blank"><i class="fa fa-linkedin text-3xl p-2 font-bold" title="Let's connect"></i></a>
-                <p className="font-source-code font-bold text-4xl">/&#62;</p>
             </header>
-            <img className="float-right h-56 rounded-lg border-2 mr-28" src="https://www.pngitem.com/pimgs/m/633-6334863_chennai-central-line-drawing-hd-png-download.png"></img>
-            <p className="text-gray-600 pb-2 ml-6">I'm always open to collaborating on projects. If you too are, then hit me up!</p>
-            <input type="text" placeholder="Name" className="ml-6 w-3/12 p-2 border-b-2 rounded-lg border-gray-600  mr-8 focus:outline-none focus:border-green-500 bg-transparent" />
-            <input type="text" placeholder="Email" className=" w-3/12 p-2 border-b-2 rounded-lg border-gray-600  mr-16 focus:outline-none focus:border-green-500  bg-transparent" />
-            <br />
-            <textarea type="text" placeholder="Message" className="ml-6 w-4/12 p-2 border-b-2 rounded-lg  mt-6 h-20 border-gray-600 focus:outline-none focus:border-green-500 bg-transparent" />
-            <button className="bg-gray-500 m-16 font-mono tracking-widest p-1 rounded-md px-20 text-white">SEND</button>
+            <p className="text-gray-600 pb-2">I'm always open to collaborating on projects. If you too are, then hit me up!</p>
         </section>
     );
 }
--- src/ProjectList.js
+++ src/ProjectList.js
@@ -1,13 +1,30 @@
+import ModalImage from "react-modal-image";
+
+
 export default function ProjectList(props) {
     const projects = props.list.map((prj, index) =>
-        <li className="flex-shrink-0 w-64 hover:scale-105" key={index.toString()}>
-            <img className="grayscale z-0 rounded-md w-64 h-60 border-2 border-dotted" src={prj.imgUrl} />
-            <p className="mt-2 text-sm font-bold z-40 text-center rounded-b-md">{prj.title}</p>
+        <li className="flex-shrink-0 w-64" key={index.toString()}>
+            <ModalImage
+                className="grayscale rounded-md w-64 h-60 border-2 border-dotted hover:scale-125"
+                small={prj.imgUrl}
+                large={prj.popUpImg}
+                alt="Hello World!"
+                title="preview"
+            />
+            <div className="flex gap-2 m-3 justify-center">
+                <p className="mt-2 text-sm font-bold z-40 text-center rounded-b-md">{prj.title}</p>
+                <button className="p-1 bg-red-400 text-white text-xs rounded-md scale-90 hover:scale-100">
+                    <a href={prj.codeUrl} target="_blank">
+                        View Code
+                    </a>
+                </button>
+            </div>
+
         </li>
     );
 
     return (
-        <ul className=" h-72 mt-6 gap-16 mx-8 my-4 flex overflow-x-auto space-x-10 rounded-md">
+        <ul className=" h-80 mt-6 gap-16 mx-8 my-4 flex overflow-x-auto space-x-10 rounded-md">
             {projects}
         </ul>
     );
